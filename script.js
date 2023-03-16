b6304a0a4e818463acd1af9da2dede01d85ab17a
Sambit_Sahoo <93509732+Sahoo-Sambit@users.noreply.github.com>
2023-03-16 23:55:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-let yourArray = ['one', 2, 'three', true, false, undefined, null];
-console.log(yourArray);
+const list = document.querySelectorAll('li');
+let yourArray = ['one', 2, true, 56, null];
+for (let i = 0; i < 5; i++) {
+	var element=yourArray[i];
+	list[i].innerHTML=element;
+}
